7c6fd5a744100909b571475fe89c0ce0eb66ae4d
Perry Sittser <sittserp@gmail.com>
2021-02-26 17:05:29 -0800

    server folder

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const transporter = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.json());
+app.use(express.static(buildPath));
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'build', 'src/index.js'));
+});
+
+app.get('/ping', (req, res) => {
+  return res.send('pong');
+});
+
+app.post('/send', (req, res) => {
+  try {
+    const mailOptions = {
+      form: req.body.email,
+      to: process.env.email,
+      subject: req.body.subject,
+      html: `
+          <p>You have a new contact request.</p>
+          <h3>Contact Details</h3>
+          <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Subject: ${req.body.subject}</li>
+            <li>Message: ${req.body.name}</li>
+        </ul>
+            `
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if(err) {
+        res.status(500).send({
+          success: false,
+          message: 'Something went wrong. Try again later'
+        });
+      } else {
+        res.send({
+          success: true,
+          message: 'Thanks for contacting us! We\'ll get back to you shortly'
+        });
+      }
+    });
+  } catch(error) {
+    res.status(500).send({
+      success: false,
+      message: 'Something went wrong. Try again later'
+    });
+  }
+});
+
+app.listen(7891, () => {
+  console.log('CORS-enabled server listening on port 7891');
+});
